617b7e55072a805c890e3ec22685d8eebad3db1a
Test <ksyuzlo7073@gmail.com>
2025-05-03 22:35:04 +1000

    incom

---
+++ task4.py
@@ -0,0 +1,35 @@
+import types
+
+
+def flat_generator(list_of_list):
+    stack = list(list_of_list)[::-1]
+    while stack:
+        current = stack.pop()
+        if isinstance(current, list):
+            stack.extend(current[::-1])
+        else:
+            yield current
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
